7bf4d351690bc6e260ab093affee022abf05b7bb
Danishkar <danishkar@limarktech.com>
2024-04-10 16:05:27 +0530

    feat: adds a slidable widget to delete a todo

--- lib/utils/todo_list.dart
+++ lib/utils/todo_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 // import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoList extends StatelessWidget {
@@ -24,36 +25,48 @@ class TodoList extends StatelessWidget {
         right: 20,
         bottom: 0,
       ),
-      child: Container(
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Colors.deepPurple,
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Row(
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: const ScrollMotion(),
           children: [
-            Checkbox(
-              value: taskCompleted,
-              onChanged: onChanged,
-              checkColor: Colors.black,
-              activeColor: Colors.white,
-              side: const BorderSide(
-                color: Colors.white,
-              ),
-            ),
-            Text(
-              taskName,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-                decoration: taskCompleted
-                    ? TextDecoration.lineThrough
-                    : TextDecoration.none,
-                decorationColor: Colors.white,
-                decorationThickness: 2,
-              ),
+            SlidableAction(
+              onPressed: deleteFunction,
+              icon: Icons.delete,
+              borderRadius: BorderRadius.circular(15),
             ),
           ],
+        ),
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.deepPurple,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Row(
+            children: [
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                checkColor: Colors.black,
+                activeColor: Colors.white,
+                side: const BorderSide(
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                taskName,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  decoration: taskCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                  decorationColor: Colors.white,
+                  decorationThickness: 2,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
